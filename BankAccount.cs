eed8c45a7867a2ac03f6f3b85d5222741e2cc7fe
JipVCurio <jv49@curio.nl>
2024-11-06 17:23:05 +0100

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,34 @@
+﻿
+class BankAccount
+{
+    private decimal balance;
+
+    public BankAccount()
+    {
+        balance = 100;
+    }
+
+    public decimal getBalance()
+    {
+        return balance;
+    }
+
+    public void addToBalance(decimal amount)
+    {
+        balance += amount;
+    }
+
+    public bool subtractFromBalance(decimal amount)
+    {
+        if (amount < balance)
+        {
+            balance -= amount;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
---
+++ main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //Create account
+        BankAccount account = new BankAccount();
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Huidig saldo: " + account.getBalance());
+            Console.WriteLine("Geld opnemen (voer bedrag in): ");
+
+            decimal amount = Convert.ToDecimal(Console.ReadLine());
+            account.subtractFromBalance(amount);
+        }
+
+    }
+}
